9321a34972951819add038df5d670f839a644052
Dian <dian@petanikode.com>
2023-12-13 11:03:00 +0800

    Switch, Ramp, Launcher

--- Assets/Scripts/LauncherController.cs
+++ Assets/Scripts/LauncherController.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,22 @@ public class LauncherController : MonoBehaviour
 
     public Collider bola;
     public KeyCode input;
-    public float force; 
+    public float maxTimeHold;
+    public float maxForce;
+    public float force;
+
+    public Material holdingMaterial;
+    public Material idleMaterial; 
+
+    // button holding state
+    private bool isHold = false;
+
+    private Renderer renderer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -31,9 +42,35 @@ public class LauncherController : MonoBehaviour
 
     private void ReadInput(Collider collider)
     {
-        if(Input.GetKey(input))
+        if(Input.GetKey(input) && !isHold)
         {
-            collider.GetComponent<Rigidbody>().AddForce(Vector3.forward * force); 
+            StartCoroutine(StartHold(collider));
         }
+    }
+
+    private IEnumerator StartHold(Collider collider)
+    {
+        isHold = true;
+        float force = 0.0f;
+        float timeHold = 0.0f;
+
+        // set launcher renderer
+        renderer.material = holdingMaterial;
+
+        while(Input.GetKey(input))
+        {
+            // kalkulasi force
+            force = Mathf.Lerp(0, maxForce, timeHold / maxTimeHold);
+
+            yield return new WaitForEndOfFrame();
+            timeHold += Time.deltaTime;
+        }
+        
+        // move ball
+        collider.GetComponent<Rigidbody>().AddForce(Vector3.forward * force);
+        isHold = false;
+
+        // set launcher render
+        renderer.material = idleMaterial;
     }
 }
